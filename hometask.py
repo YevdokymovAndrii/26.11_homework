765bf968ebc6049580fb1d6f11914aa9fef1e970
YevdokymovAndrii <ayevdokymov@kse.org.ua>
2024-11-26 21:48:52 +0200

    function values show functions

--- hometask.py
+++ hometask.py
@@ -13,3 +13,12 @@ while True:
     except TypeError:
         pass
 
+log_x, log_y, sq_x, sq_y = [], [], [], []
+for i in range(-100, 100):
+    log_x.append(i)
+    log_y.append(np.log2(i))
+    sq_x.append(i)
+    sq_y.append(f(i))
+mp.plot(log_x, log_y)
+mp.plot(sq_x, sq_y)
+mp.show()
